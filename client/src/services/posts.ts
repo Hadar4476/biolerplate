0e6e86b09bdf98605459a1760cb179cc55977159
hadar4476 <hadar4476@gmail.com>
2024-11-18 20:39:43 +0200

    changes

--- client/src/services/posts.ts
+++ client/src/services/posts.ts
@@ -1,23 +1,32 @@
 import apiClient from "@/api-client";
 
-import { IPost, ICreatePostRequest } from "@/types";
+import { IPost, ICreatePostRequest, ROUTE_NAMES } from "@/types";
 
 export const fetchPosts = async (): Promise<IPost[]> => {
-  const response = await apiClient.get<IPost[]>("/posts");
+  const response = await apiClient.get<IPost[]>(ROUTE_NAMES.POSTS);
+
+  return response.data;
+};
+
+export const fetcgPostById = async (postId: IPost["_id"]): Promise<IPost[]> => {
+  const response = await apiClient.get<IPost[]>(
+    `${ROUTE_NAMES.POSTS}/${postId}`
+  );
+
   return response.data;
 };
 
 export const createPost = async (
   postData: ICreatePostRequest
 ): Promise<IPost> => {
-  const response = await apiClient.post<IPost>("/posts", postData);
+  const response = await apiClient.post<IPost>(ROUTE_NAMES.POSTS, postData);
 
   return response.data;
 };
 
 export const updatePost = async (postData: IPost): Promise<IPost> => {
   const response = await apiClient.put<IPost>(
-    `/posts/${postData._id}`,
+    `${ROUTE_NAMES.POSTS}/${postData._id}`,
     postData
   );
 
@@ -27,7 +36,9 @@ export const updatePost = async (postData: IPost): Promise<IPost> => {
 export const deletePost = async (
   postId: IPost["_id"]
 ): Promise<IPost["_id"]> => {
-  const response = await apiClient.delete<IPost["_id"]>(`/posts/${postId}`);
+  const response = await apiClient.delete<IPost["_id"]>(
+    `${ROUTE_NAMES.POSTS}/${postId}`
+  );
 
   return response.data;
 };

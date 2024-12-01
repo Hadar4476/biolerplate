9b4a10f925209e4320b32de9d653f91ab7e1fed7
Hadar4476 <hadar4476@gmail.com>
2024-12-01 13:58:51 +0200

    changes

--- client/src/api-client.ts
+++ client/src/api-client.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 const apiClient = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: apiUrl,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
